5f6d98386515a63e5c70f2c46119037056c63ce1
Max <maxnatanaelstk@gmail.com>
2021-12-05 21:50:16 -0300

    wallet form layout

--- finances/forms/wallet/wallet_create_form.py
+++ finances/forms/wallet/wallet_create_form.py
@@ -1,6 +1,8 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Div
 from django import forms
+from django.utils.safestring import mark_safe
+
 
 from finances.models import Wallet
 
@@ -12,22 +14,25 @@ class WalletCreateForm(forms.ModelForm):
         fields = '__all__'
         widgets = {'user': forms.HiddenInput()}
 
-    name = forms.CharField(label='Nome da Carteira', max_length=50)
+    name = forms.CharField(label=mark_safe('<b>Nome da Carteira</b>'), max_length=50)
     description = forms.CharField(label='Descrição', max_length=500, widget=forms.Textarea())
-    balance = forms.FloatField(label='Saldo inicial da carteira')
+    balance = forms.FloatField(label=mark_safe('<b>Saldo inicial da carteira</b>'))
 
     def __init__(self, *args, **kwargs):
         super(WalletCreateForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.layout = Layout(
-
-        )
-
+        self.helper.layout = self.build_layout()
         self.helper.add_input(Submit('submit', 'Salvar'))
 
     def build_layout(self):
         return Layout(
             Div(
-
+                Div('name', css_class='col-lg-8'),
+                Div('balance', css_class='col-lg-4'),
+                css_class='row'
+            ),
+            Div(
+                Div('description', css_class='col-lg-12'),
+                css_class='row'
             )
         )
