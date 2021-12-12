2d56573d9d69f1181e2b642a1534ea3347330815
Max <maxnatanaelstk@gmail.com>
2021-12-11 23:59:00 -0300

    login required

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 
 from finances.views import HomeView
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls', namespace='account')),
-    path('finances/', include('finances.urls', namespace='finances')),
+    path('account/', login_required(include('account.urls', namespace='account'))),
+    path('finances/', login_required(include('finances.urls', namespace='finances'))),
     path('', HomeView.as_view(), name='home'),
 ]
