95e5dcd52bbc1e570fe39da6c8a3a18caccf0050
Max <maxnatanaelstk@gmail.com>
2021-12-12 22:52:56 -0300

    remoção dos arquivos desnecessaários do static

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -1,8 +1,9 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
 from finances.views import HomeView
-
 
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('account/', include('account.urls', namespace='account')),
     path('finances/', include('finances.urls', namespace='finances')),
     path('', HomeView.as_view(), name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
