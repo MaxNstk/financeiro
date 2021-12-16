7353db8bfd33e11ec6ca3804520e68dfb73ecef6
Max <maxnatanaelstk@gmail.com>
2021-12-15 23:26:59 -0300

    register form

--- account/forms/register.py
+++ account/forms/register.py
@@ -1,18 +1,19 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Button
+from crispy_forms.layout import Submit, Layout, Div
 from django import forms
 
 from account.models.user import User
-from finances.forms.generic.custom_model_form import CustomModelForm
 
 
 class RegisterForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'phone', 'email']
+        fields = ['first_name', 'last_name', 'username', 'phone', 'email', 'password', 'password2']
 
-    username = forms.CharField(max_length=20, label='Usuário')
+    first_name = forms.CharField(max_length=50, label='Primeiro Nome')
+    last_name = forms.CharField(max_length=50, label='Sobrenome')
+    username = forms.CharField(max_length=20, label='Nome de usuário', help_text='Nome único que será utilizado dentro do sistema')
     email = forms.EmailField(label='Email')
     phone = forms.CharField(max_length=20, label='Telefone')
     password = forms.CharField(widget=forms.PasswordInput, label='Informe sua senha')
@@ -21,8 +22,8 @@ class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        # self.helper.layout = self.build_layout()
-        self.helper.add_input(Submit('submit', 'Salvar'))
+        self.helper.layout = self.build_layout()
+        self.helper.add_input(Submit('submit', 'Salvar'),)
 
     def clean_username(self):
 
@@ -49,4 +50,24 @@ class RegisterForm(forms.ModelForm):
         return cl['password2']
 
     def build_layout(self):
-        return {}
+        return Layout(
+            Div(
+                Div('first_name', css_class='col-lg-5'),
+                Div('last_name', css_class='col-lg-7'),
+                css_class='row'
+            ),
+            Div(
+                Div('username', css_class='col-lg-7'),
+                Div('phone', css_class='col-lg-5'),
+                css_class='row'
+            ),
+            Div(
+                Div('email', css_class='col-lg-12'),
+                css_class='row'
+            ),
+            Div(
+                Div('password', css_class='col-lg-6'),
+                Div('password2', css_class='col-lg-6'),
+                css_class='row'
+            )
+        )
