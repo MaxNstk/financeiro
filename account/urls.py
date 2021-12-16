a03663ae74fd33675c17c08497cb353e3e808c21
Max <maxnatanaelstk@gmail.com>
2021-12-15 23:10:50 -0300

    Login e register templates

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from account.views.authentication.register import RegisterView
@@ -5,5 +6,8 @@ from account.views.authentication.register import RegisterView
 app_name = 'account'
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-]
+    path('register/', RegisterView.as_view(template_name='account/register.html'), name='register'),
+    path('login/', LoginView.as_view(template_name='account/login.html', success_url='home'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    ]
+
