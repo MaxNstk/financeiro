a6e635f19011db9dcaf1d54c332220c20178294c
max <maxnatanaelstk@gmail.com>
2022-01-03 21:48:53 -0300

    update view protection

--- finances/views/generic/custom_update_view.py
+++ finances/views/generic/custom_update_view.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.views.generic import UpdateView
 
 
@@ -8,3 +9,15 @@ class CustomUpdateView(UpdateView):
         ctx = super(CustomUpdateView, self).get_context_data()
         ctx['breadcrumbs'] = self.breadcrumbs
         return ctx
+
+    def get(self, request, *args, **kwargs):
+        if self.model.objects.get(id=kwargs['pk']).user != request.user:
+            return redirect('home')
+        else:
+            return super(CustomUpdateView, self).get(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if self.model.objects.get(id=kwargs['pk']).user != request.user:
+            return redirect('home')
+        else:
+            return super(CustomUpdateView, self).get(self, request, *args, **kwargs)
